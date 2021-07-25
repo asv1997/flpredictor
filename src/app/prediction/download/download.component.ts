ae2f66019f70eae2e6a7985aecda3c977e1e8492
asv1997 <asvignesh1997@gmail.com>
2021-07-25 22:18:46 +0530

    added progress bar and plug link in the league table

--- src/app/prediction/download/download.component.ts
+++ src/app/prediction/download/download.component.ts
@@ -17,26 +17,35 @@ export class DownloadComponent implements OnInit {
   league:any
   table:Object[]=[];
   date:Date;
+  isDownloading:number;
 
   constructor(private predictionService: PredictionService, private router : Router) {
 
    //to ensure that the user cant directly access the page without using the choose-league component
    this.name = this.predictionService.getName();
-    if(this.name==="" || this.name.length ==0){
-      router.navigateByUrl("/")
-    }
+    // if(this.name==="" || this.name.length ==0){
+    //   router.navigateByUrl("/")
+    // }
 
+    this.isDownloading = 0;
     this.date = new Date();
     this.leagueId = this.predictionService.getLeagueId();
     this.league = this.predictionService.getLeague(this.leagueId-1);
     this.table = this.predictionService.getPredictedTable();
   }
 
-  download(){
-    domtoimage.toBlob(document.getElementById('table-container'))
+ async download(){
+    this.isDownloading = 1;
+    await domtoimage.toBlob(document.getElementById('table-container'))
     .then(function(blob) {
       saveAs(blob, 'prediction.png');
+
     });
+    this.isDownloading = 2;
+
+    setTimeout(() => {
+      this.isDownloading = 0;
+    }, 1000);
   }
 
 
